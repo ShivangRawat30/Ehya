33aab9819ebda15a3bbed82230212215a8b61b53
Shivang Rawat <rawatshivang30@gmail.com>
2023-05-08 23:34:25 +0530

    commit1

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { images } from "../constants";
 
 const Header = () => {
   return (
     <section>
-      <header></header>
+      <header>
+        <div>
+          <img src={images.Logo} alt="logo" />
+        </div>
+        <div></div>
+      </header>
     </section>
   );
 };

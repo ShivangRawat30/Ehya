fa02249295700e0d506072040072b5e394929582
Shivang Rawat <rawatshivang30@gmail.com>
2023-05-13 23:38:36 +0530

    h

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -31,12 +31,12 @@ const Header = () => {
     });
   };
   return (
-    <section>
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
         </div>
-        <div>
+        <div className="lg:hidden z-50">
           {navIsVisible ? (
             <AiOutlineClose
               className="w-6 h-6"
@@ -49,9 +49,9 @@ const Header = () => {
         <div
           className={`${
             navIsVisible ? "right-0" : "-right-full"
-          }flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
         >
-          <ul className="flex gap-x-5 font-semibold">
+          <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemsInfo.map((item) => (
               <NavItem key={item.name} name={item.name} />
             ))}

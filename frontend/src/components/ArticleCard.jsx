a92833d46fd71094a46667af24120c0471cd286d
Shivang Rawat <rawatshivang30@gmail.com>
2023-05-18 23:22:20 +0530

    article card added

---
+++ frontend/src/components/ArticleCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { images } from "../constants";
+
+const ArticleCard = ({ className }) => {
+  return (
+    <div
+      className={` rounded-x1 overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] `}
+    >
+      <img
+        src={images.Post1Image}
+        alt="title"
+        className="w-full object-cover object-center h-auto"
+      />
+      <div className="p-5">
+        <h2 className="font-roboto font-bold text-xl text-dark-soft">
+          Future of Work
+        </h2>
+        <p className="text-dark-light mt-3 text-sm">
+          Majority of people will work on jobs that don't exist today.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -71,7 +71,7 @@ const Header = () => {
     <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
-          <img className="w-16" src={images.Logo} alt="logo" />
+          <img className="w-20" src={images.Logo} alt="logo" />
         </div>
         <div className="lg:hidden z-50">
           {navIsVisible ? (
